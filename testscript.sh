369e8f7de86f10aeed879ea7d49b9fb484f5471c
meir meiry <meir.yanovich@personetics.com>
2020-01-21 08:47:42 +0000

    Test commit4

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is new commit from meiry"
+echo "This is new commit from meiry2"
 echo "this was run automagically"
